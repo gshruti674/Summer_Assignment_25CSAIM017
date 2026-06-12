99e4bc15337cf9cf1dc67e60cbb401b02b595446
Shruti Gupta <gshruti674@gmail.com>
2026-06-12 10:19:52 +0530

    Day 9 uploaded

---
+++ Day-9/charpattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("enter no.");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",'A'+i-1);
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
